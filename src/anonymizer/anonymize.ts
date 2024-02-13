7e618e9daf97c2ed28ed10c9065eb49a2382e548
mjorocio <mj.dejarcio@gmail.com>
2024-02-13 16:24:27 +0800

    Allow-Origin

--- src/anonymizer/anonymize.ts
+++ src/anonymizer/anonymize.ts
@@ -1,15 +1,29 @@
 export async function anonymizer(prompt: string) {
-    const response = await fetch(
-        "http://127.0.0.1:5000/anonymize",
-        {
-            headers: {
-                "Content-Type": "application/json",
-                'Access-Control-Allow-Origin': '*'
+    // const response = await fetch(
+    //     "http://127.0.0.1:5000/anonymize",
+    //     {
+    //         headers: {
+    //             "Content-Type": "application/json",
+    //             'Access-Control-Allow-Origin': '*'
+    //         },
+    //         method: "POST",
+    //         body: JSON.stringify({text: prompt})
+    //     }
+    // );
+    // const result = await response.json();
+    // return result;
+
+    const response = await fetch("http://127.0.0.1:5000/anonymize", {
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+              'Content-Type': 'application/json',
+              'Access-Control-Allow-Origin': '*'
             },
-            method: "POST",
-            body: JSON.stringify({text: prompt})
-        }
-    );
+      body: JSON.stringify({text: prompt}),
+    });
+
     const result = await response.json();
     return result;
 }
+
